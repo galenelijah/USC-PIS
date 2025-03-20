65108dbdf95f30861cf16cfcb0d42102d346d141
Galen Elijah Sabequil <sgalenelijah@gmail.com>
2025-03-20 21:54:18 +0800

    Trying something else

--- backend/frontend/frontend/vite.config.js
+++ backend/frontend/frontend/vite.config.js
@@ -4,16 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/static/',
   build: {
     outDir: 'dist',
-    assetsDir: 'static',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'static/[name].[ext]',
-        chunkFileNames: 'static/[name].js',
-        entryFileNames: 'static/[name].js',
-      },
-    },
+    emptyOutDir: true,
   },
 })
