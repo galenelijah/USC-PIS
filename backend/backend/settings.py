493116063d98a3375bde80e5d455734e6fbb11d9
Galen Elijah Sabequil <sgalenelijah@gmail.com>
2025-03-22 22:52:51 +0800

    There was a problem

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -134,9 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
+STATICFILES_DIRS = []  # Remove STATIC_ROOT from here
 
 # Whitenoise settings
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
